f1ae73c02d64321fad19dfdc399309273532caa1
Tushar Kewat <159452915+tusharkewat@users.noreply.github.com>
2026-03-31 14:53:54 +0530

    Solution

---
+++ Sort_Colors.java
@@ -0,0 +1,34 @@
+// LeetCode 75. Sort Colors
+// Complexity
+// Time: O(n) 
+// Space: O(1) 
+
+public class Sort_Colors {
+    public static void sortColors(int[] arr) {
+        int low = 0, mid = 0, high = arr.length - 1;
+        while (mid <= high) {
+            if( arr[mid] == 0){
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            } else if( arr[mid] == 1){
+                mid++;
+            } else {
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        int arr[] = {2,0,2,1,1,0};
+        sortColors(arr);
+        for ( int i = 0; i < arr.length; i++) {
+            System.err.println(arr[i]);
+        }
+    }
+}
