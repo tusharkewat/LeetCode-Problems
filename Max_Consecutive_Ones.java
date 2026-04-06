a629af3942a7f6c1b5a7a408b10c7517d40b496a
Tushar Kewat <159452915+tusharkewat@users.noreply.github.com>
2026-04-06 19:45:13 +0530

    Using Counter

---
+++ Max_Consecutive_Ones.java
@@ -0,0 +1,24 @@
+// LeetCode 485. Max Consecutive Ones
+// Compexity 
+// Time  O(n)
+// Space  O(1)
+
+public class Max_Consecutive_Ones {
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int maxCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+                maxCount = Math.max(maxCount, count);
+            } else {
+                count = 0;
+            }
+        }
+        return maxCount;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,1,0,1,1,1};
+        System.err.println(findMaxConsecutiveOnes(arr));
+    }
+}
