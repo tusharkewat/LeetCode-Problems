2d3e51486e99bc2628cb32491201405a0a9dd72f
Tushar Kewat <159452915+tusharkewat@users.noreply.github.com>
2026-04-14 13:40:05 +0530

    Solution

--- Intersection_of_Two_Arrays_2.java
+++ Intersection_of_Two_Arrays_2.java
@@ -5,7 +5,6 @@
 // Sorting	 O(n log n + m log m)	O(1)
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,52 +13,52 @@ public class Intersection_of_Two_Arrays_2 {
     public static int[] intersect(int[] nums1, int[] nums2) {
         // Using HashMap = Counting frequency 
 
-        // Map <Integer, Integer> map = new HashMap<>();
+        Map <Integer, Integer> map = new HashMap<>();
 
-        // for (int i : nums1) {
-        //     map.put(i, map.getOrDefault(i, 0) + 1);
-        // }
+        for (int i : nums1) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
 
-        // List <Integer> result = new ArrayList<>();
-        // for (int i : nums2) {
-        //     if (map.containsKey(i) && map.get(i) > 0) {
-        //         result.add(i);
-        //         map.put(i, map.get(i) - 1);
-        //     }
-        // }
-        // int arr[] = new int[result.size()];
-        // for (int i = 0; i < result.size(); i++) {
-        //     arr[i] = result.get(i);
-        // }
+        List <Integer> result = new ArrayList<>();
+        for (int i : nums2) {
+            if (map.containsKey(i) && map.get(i) > 0) {
+                result.add(i);
+                map.put(i, map.get(i) - 1);
+            }
+        }
+        int arr[] = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            arr[i] = result.get(i);
+        }
 
-        // return arr;
+        return arr;
 
 
         // Using Sorting + Two Pointer
 
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+        // Arrays.sort(nums1);
+        // Arrays.sort(nums2);
 
-        int i = 0, j = 0;
-        List<Integer> result = new ArrayList<>();
+        // int i = 0, j = 0;
+        // List<Integer> list = new ArrayList<>();
 
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] == nums2[j]) {
-                result.add(nums1[i]);
-                i++;
-                j++;
-            } else if (nums1[i] < nums2[j]) {
-                i++;
-            } else {
-                j++;
-            }
-        }
+        // while (i < nums1.length && j < nums2.length) {
+        //     if (nums1[i] == nums2[j]) {
+        //         list.add(nums1[i]);
+        //         i++;
+        //         j++;
+        //     } else if (nums1[i] < nums2[j]) {
+        //         i++;
+        //     } else {
+        //         j++;
+        //     }
+        // }
 
-        int[] res = new int[result.size()];
-        for (int k = 0; k < result.size(); k++) {
-            res[k] = result.get(k);
-        }
+        // int[] res = new int[result.size()];
+        // for (int k = 0; k < result.size(); k++) {
+        //     res[k] = result.get(k);
+        // }
 
-        return res;
+        // return res;
     }
 }
