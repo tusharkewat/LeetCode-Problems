3ea1da832994ad2019fca6e94f94e9fef1563038
Tushar Kewat <159452915+tusharkewat@users.noreply.github.com>
2026-04-17 14:46:18 +0530

    Using Extra Space

---
+++ Rotate_Array.java
@@ -0,0 +1,15 @@
+public class Rotate_Array {
+    public void rotate(int[] nums, int k) {
+        // Using extra space   O(n)
+        int n = nums.length;
+        int[] temp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            temp[(i + k) % n] = nums[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = temp[i];
+        }
+    }
+}
