7d42daadf303f06438f39ab119119cca7e06f117
Tushar Kewat <159452915+tusharkewat@users.noreply.github.com>
2026-04-19 18:15:38 +0530

    Using HashSet

---
+++ Find_the_Duplicate_Number.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+public class Find_the_Duplicate_Number {
+    public int findDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return nums[i];
+            } else [
+                set.add(nums[i]);
+            ]
+        }
+        return -1;
+    }
+}
