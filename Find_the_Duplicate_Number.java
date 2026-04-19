22d7cea404381f5a8e5b25d92ec7418237e001a6
Tushar Kewat <159452915+tusharkewat@users.noreply.github.com>
2026-04-19 18:21:40 +0530

    Using Floyd’s Cycle Detection

--- Find_the_Duplicate_Number.java
+++ Find_the_Duplicate_Number.java
@@ -1,15 +1,44 @@
-import java.util.HashSet;
+// LeetCode 287. Find the Duplicate Number
+// Complexity 
+// Time   O(n)
+// Space  O(1)
+
+// import java.util.HashSet;
 
 public class Find_the_Duplicate_Number {
-    public int findDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (set.contains(nums[i])) {
-                return nums[i];
-            } else [
-                set.add(nums[i]);
-            ]
+    // Using Floyd’s Cycle Detection
+    public static int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return -1;
+
+        return slow;
     }
+    public static void main(String[] args) {
+        int arr[] = {1,3,4,2,2};
+        System.out.println(findDuplicate(arr));
+    }
+
+    // Using HashSet
+    // public int findDuplicate(int[] nums) {
+    // HashSet<Integer> set = new HashSet<>();
+    // for (int i = 0; i < nums.length; i++) {
+    // if (set.contains(nums[i])) {
+    // return nums[i];
+    // } else {
+    // set.add(nums[i]);
+    // }
+    // }
+    // return -1;
+    // }
 }
