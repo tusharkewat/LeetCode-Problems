efe6388406a7719c3b21496968e555c173c3a3f8
Tushar Kewat <159452915+tusharkewat@users.noreply.github.com>
2026-04-22 23:31:04 +0530

    Solution

--- Reverse_Words_in_a_String.java
+++ Reverse_Words_in_a_String.java
@@ -1,19 +1,66 @@
 // LeetCode 151. Reverse Words in a String
 // Complexity 
 // Time   O(n)
-// Space  O(n)
+
 
 public class Reverse_Words_in_a_String {
-    public String reverseWords(String s) {
-        String[] words = s.trim().split("\\s+");
-        StringBuilder result = new StringBuilder();
+    // Using Built-in Functions
+    // Space  O(n)
 
-        for (int i = words.length - 1; i >= 0; i--) {
-            result.append(words[i]);
-            if (i != 0)
-                result.append(" ");
+    // public String reverseWords(String s) {
+    //     String[] words = s.trim().split("\\s+");
+    //     StringBuilder result = new StringBuilder();
+
+    //     for (int i = words.length - 1; i >= 0; i--) {
+    //         result.append(words[i]);
+    //         if (i != 0)
+    //             result.append(" ");
+    //     }
+
+    //     return result.toString();
+    // }
+
+    // Using Two Pointer
+    // Space  O(n)
+
+    public String reverseWords(String s) {
+        char[] arr = s.toCharArray();
+
+        reverse(arr, 0, arr.length - 1);
+
+        int start = 0;
+        for (int end = 0; end <= arr.length; end++) {
+            if (end == arr.length || arr[end] == ' ') {
+                reverse(arr, start, end - 1);
+                start = end + 1;
+            }
         }
 
-        return result.toString();
+        return cleanSpaces(arr);
+    }
+
+    private void reverse(char[] arr, int l, int r) {
+        while (l < r) {
+            char temp = arr[l];
+            arr[l++] = arr[r];
+            arr[r--] = temp;
+        }
+    }
+
+    private String cleanSpaces(char[] arr) {
+        int i = 0, j = 0, n = arr.length;
+
+        while (j < n) {
+            while (j < n && arr[j] == ' ')
+                j++;
+            while (j < n && arr[j] != ' ')
+                arr[i++] = arr[j++];
+            while (j < n && arr[j] == ' ')
+                j++;
+            if (j < n)
+                arr[i++] = ' ';
+        }
+
+        return new String(arr, 0, i);
     }
 }
