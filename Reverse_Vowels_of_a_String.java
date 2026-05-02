1826383e4f75eb0ce321b532dd5ef3a8dd705241
Tushar Kewat <159452915+tusharkewat@users.noreply.github.com>
2026-05-02 21:40:02 +0530

    Solution

---
+++ Reverse_Vowels_of_a_String.java
@@ -0,0 +1,31 @@
+public class Reverse_Vowels_of_a_String {
+    public String reverseVowels(String s) {
+        char[] arr = s.toCharArray();
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            while (left < right && !isVowel(arr[left])) {
+                left++;
+            }
+
+            while (left < right && !isVowel(arr[right])) {
+                right--;
+            }
+
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(arr);
+    }
+
+    private boolean isVowel(char c) {
+        c = Character.toLowerCase(c);
+        return c == 'a' || c == 'e' || c == 'i' ||
+                c == 'o' || c == 'u';
+    }
+}
