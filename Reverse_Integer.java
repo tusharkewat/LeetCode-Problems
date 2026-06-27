0709b6bcd466774e0fa62161a709080c54b3b76d
Tushar Kewat <159452915+tusharkewat@users.noreply.github.com>
2026-06-27 19:21:45 +0530

    Solution

---
+++ Reverse_Integer.java
@@ -0,0 +1,27 @@
+// LeetCode 7. Reverse Integer
+// Complexity
+// Time  O(log n)
+// Space  O(1)
+
+public class Reverse_Integer {
+    public static int reverse(int x) {
+        long reverse = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x = x / 10;
+        }
+        if (reverse > Integer.MAX_VALUE ||
+            reverse < Integer.MIN_VALUE) {
+                return 0;
+            }
+        
+        return (int) reverse;
+    }
+
+    public static void main(String[] args) {
+        int num = 120;
+        System.out.println(reverse(num));
+    }
+}
