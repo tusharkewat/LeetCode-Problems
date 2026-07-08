a22dd4beb154b5f642a75bec161d3107b4ebf57f
Tushar Kewat <159452915+tusharkewat@users.noreply.github.com>
2026-07-08 21:52:26 +0530

    Solution

---
+++ Swap_Nodes_in_Pairs.java
@@ -0,0 +1,28 @@
+// LeetCode 24. Swap Nodes in Pairs
+// Complexity
+// Time  O(n)
+// Space  O(1)
+
+public class Swap_Nodes_in_Pairs {
+    public ListNode swapPairs(ListNode head) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+
+        while (prev.next != null && prev.next.next != null) {
+
+            ListNode first = prev.next;
+            ListNode second = first.next;
+
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+
+            prev = first;
+        }
+
+        return dummy.next;
+    }
+}
