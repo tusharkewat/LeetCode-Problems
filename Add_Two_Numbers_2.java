5650515395c1cfcc4fea032f640931dbadf1b9e7
Tushar Kewat <159452915+tusharkewat@users.noreply.github.com>
2026-07-12 16:30:20 +0530

    Solution

---
+++ Add_Two_Numbers_2.java
@@ -0,0 +1,48 @@
+// LeetCode 445. Add Two Numbers II
+// Complexity
+// Time  O(n)
+// Space  O(1)
+
+public class Add_Two_Numbers_2 {
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+
+        ListNode head = null;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            ListNode node = new ListNode(sum % 10);
+            node.next = head;
+            head = node;
+            carry = sum / 10;
+        }
+
+        return head;
+    }
+}
