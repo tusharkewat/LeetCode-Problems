bcbc0eabad986cf4ec41269e3560c3681114a1c9
Tushar Kewat <159452915+tusharkewat@users.noreply.github.com>
2026-07-19 13:15:59 +0530

    Stack

---
+++ Backspace_String_Compare.java
@@ -0,0 +1,30 @@
+// LeetCode 844. Backspace String Compare
+// Complexity
+// Time: O(n + m)
+// Space: O(n + m)
+
+
+public class Backspace_String_Compare {
+    public boolean backspaceCompare(String s, String t) {
+        String str1 = removeStars(s);
+        String str2 = removeStars(t);
+
+        return str1.equals(str2);
+    }
+
+    private String removeStars(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '#') {
+                if (sb.length() > 0) {
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
