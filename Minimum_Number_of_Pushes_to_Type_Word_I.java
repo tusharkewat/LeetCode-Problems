bc95cdf146f2aa206018745c5e63d901254644b5
Tushar Kewat <159452915+tusharkewat@users.noreply.github.com>
2026-07-30 21:13:39 +0530

    Solution

---
+++ Minimum_Number_of_Pushes_to_Type_Word_I.java
@@ -0,0 +1,22 @@
+// LeetCode 3014. Minimum Number of Pushes to Type Word I
+// Complexity
+// Time  O(n)
+// Space  O(n)
+
+public class Minimum_Number_of_Pushes_to_Type_Word_I {
+    public static int minimumPushes(String word) {
+        
+        int ans = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            ans += (i / 8) + 1;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String word = "xycdefghij";
+        System.out.println(minimumPushes(word));
+    }
+}
