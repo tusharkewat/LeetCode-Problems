456d8c4e4365b43ceaaca4d4c7ce4088e7745888
Tushar Kewat <159452915+tusharkewat@users.noreply.github.com>
2026-09-02 11:57:57 +0530

    Solution

---
+++ Construct_Uniform_Parity_Array_I.java
@@ -0,0 +1,15 @@
+// LeetCode 3875. Construct Uniform Parity Array I
+// Complexity
+// Time O(1)
+// Space O(1)
+
+public class Construct_Uniform_Parity_Array_I {
+    public static boolean uniformArray(int[] nums1) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int nums1[] = {2,3};
+        System.out.println(uniformArray(nums1));
+    }
+}
